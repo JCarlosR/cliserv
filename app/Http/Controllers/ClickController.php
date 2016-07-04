6211877630bf78539171f6c513d3573ae85f65bf
SoleSCaverO <edilberto0905@gmail.com>
2016-07-04 01:54:13 -0500

    1 report left

--- app/Http/Controllers/ClickController.php
+++ app/Http/Controllers/ClickController.php
@@ -121,8 +121,8 @@ class ClickController extends Controller
 
     public function bestProduct()
     {
-        $today  = Carbon::today();
-        $clicks = Click::whereNotNull('product_id')->where('product_id','!=',0)->where('fecha','>=',$today)->get();
+        $yesterday  = Carbon::yesterday();
+        $clicks = Click::whereNotNull('product_id')->where('product_id','!=',0)->where('fecha','>',$yesterday)->get();
 
         if( count($clicks) != 0 )
         {
@@ -180,8 +180,8 @@ class ClickController extends Controller
 
     public function bestCategory()
     {
-        $today  = Carbon::today();
-        $clicks = Click::where('fecha','>=',$today)->get();
+        $yesterday  = Carbon::yesterday();
+        $clicks = Click::where('fecha','>',$yesterday)->get();
 
 
         if( count($clicks) !=0 )
@@ -206,7 +206,7 @@ class ClickController extends Controller
                 $amount_category[$i]=0;
 
             // PROCESS CLICKS, ACCORDING TO PRODUCTS CONTAINED IN EXISTENT CATEGORIES
-            $clicks = Click::whereNotNull('product_id')->where('product_id','!=',0)->where('fecha','>=',$today)->get();
+            $clicks = Click::whereNotNull('product_id')->where('product_id','!=',0)->where('fecha','>',$yesterday)->get();
 
             foreach( $clicks as $click )
             {
