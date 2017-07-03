b289b4f05f1c54f39f75fdc965f2b2942050c77f
Juan Carlos <juancagb.17@gmail.com>
2017-07-03 14:42:12 -0500

    Update year select

--- resources/views/topproducts.blade.php
+++ resources/views/topproducts.blade.php
@@ -22,9 +22,7 @@
                     <select name="cboyear">
                         <option value="" disabled selected>Seleccionar Año</option>
                         <option value="1">Todos</option>
-                        <option value="2016">2016</option>
-                        <option value="2015">2015</option>
-                        <option value="2014">2014</option>
+                        <option value="2017">2017</option>
                     </select>
                     <label>Fecha Año</label>
                 </div>
