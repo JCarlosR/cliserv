8f14b1d6615c5eb5d9a191863450ffaae335e4a7
Juan Carlos <juancagb.17@gmail.com>
2017-07-24 14:39:09 -0500

    Remove padding left

--- resources/views/reports/products/matrix-simple.blade.php
+++ resources/views/reports/products/matrix-simple.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    <title>Top de productos - Click Stream</title>
+    <title>Matriz de horas pico - Click Stream</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
@@ -18,35 +18,25 @@
         <h4 class="center-align" id="title">Top de productos</h4>
     </div>
     <div class="container">
+
         <div class="row">
             <form id="form">
-                <div class="col s6 m3">
+                <div class="col s4">
                     <div class="input-field">
                         <input type="date" class="datepicker" name="start_date" required>
                         <label>Fecha inicio</label>
                     </div>
                 </div>
-                <div class="col s6 m3">
+                <div class="col s4">
                     <div class="input-field">
                         <input type="date" class="datepicker" name="end_date" required>
                         <label>Fecha fin</label>
                     </div>
                 </div>
-                <div class="col s6 m2">
-                    <div class="row">
-                        <div class="input-field col s12">
-                            <input type="number" required min="1" name="top" class="tooltiped"
-                                data-position="top" data-delay="20" data-tooltip="Nro de productos en el TOP">
-                            <label for="top">TOP</label>
-                        </div>
-                    </div>
-                </div>
-                <div class="col s6 m4">
-                    <button type="submit" class="waves-effect waves-light btn filter">
-                        Ver
-                    </button>
+                <div class="col s4">
+                    <button type="submit" class="waves-effect waves-light btn filter">Generar</button>
                     <button type="button" class="waves-effect waves-light btn filter" title="Exportar a Excel" id="btnToExcel">
-                        Descargar
+                        <i class="material-icons">&#xE2C0;</i>
                     </button>
                 </div>
             </form>
@@ -63,9 +53,14 @@
                     <table class="striped" id="productsTable">
                         <thead>
                         <tr>
-                            <th>Producto</th>
-                            <th># Clics</th>
-                            <th>Porcentaje</th>
+                            <th>Hora</th>
+                            <th>Domingo</th>
+                            <th>Lunes</th>
+                            <th>Martes</th>
+                            <th>Miércoles</th>
+                            <th>Jueves</th>
+                            <th>Viernes</th>
+                            <th>Sábado</th>
                         </tr>
                         </thead>
                         <tbody id="productsTop">
@@ -74,22 +69,16 @@
                     </table>
                 </div>
             </div>
-
-            <div class="row">
-                <div class="col s12">
-                    <canvas id="myChart"></canvas>
-                </div>
-            </div>
         </div>
+
     </div>
 </main>
 
 <script src="{{ asset('js/jquery.min.js') }}"></script>
 <script src="/js/materialize.min.js"></script>
 
-<script src="//cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
 <script src="/table-to-excel/index.js"></script>
-<script src="/reports/products/top.js"></script>
+<script src="/reports/products/matrix.js"></script>
 
 </body>
 </html>
