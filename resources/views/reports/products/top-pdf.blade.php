e666ea514d0923e563280f9413de323e00211f73
Juan Carlos <juancagb.17@gmail.com>
2017-07-25 23:55:48 -0500

    Inline CSS code in view

--- resources/views/reports/products/top-pdf.blade.php
+++ resources/views/reports/products/top-pdf.blade.php
@@ -4,10 +4,12 @@
     <title>Top de productos - Click Stream</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <link rel="stylesheet" href="/css/materialize.min.css">
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/styles.css') }}">
     <style>
-    main { padding-left: 0; }
+        <?php
+            include public_path().'/css/materialize.min.css';
+            include public_path().'/css/styles.css';
+        ?>
+        main { padding-left: 0; }
     </style>
 </head>
 <body>
