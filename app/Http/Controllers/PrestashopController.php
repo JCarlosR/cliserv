f5f6f424f7fefc93db7018cf7d18f6ac39e61eb7
Juan Carlos <juancagb.17@gmail.com>
2017-08-01 23:36:13 -0500

    Get by id array

--- app/Http/Controllers/PrestashopController.php
+++ app/Http/Controllers/PrestashopController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Click;
+use App\Product;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -15,9 +16,11 @@ class PrestashopController extends Controller
         $customerName = $request->input('name');
 
         // get the most viewed products for the specified user
-        $products = Click::where('user_id', $customerId)
+        $productIds = Click::where('user_id', $customerId)
             ->whereNotNull('product_id')->where('product_id','!=',0)
             ->groupBy('product_id')->pluck('product_id');
+
+        $products = Product::find($productIds);
         dd($products);
 
         return view('prestashop.index')->with(compact('customerName'));
